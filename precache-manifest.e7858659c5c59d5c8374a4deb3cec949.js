0d11922748f229932015a6cae78e41ab428ec2dc
Max Belsky <public.belsky@gmail.com>
2020-05-31 17:00:21 +0300

    Updates

---
+++ precache-manifest.e7858659c5c59d5c8374a4deb3cec949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b57320484110b6305ca9bd7008855580",
+    "url": "/react-display-name/index.html"
+  },
+  {
+    "revision": "60839cc9f8b7b5d3e6da",
+    "url": "/react-display-name/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a04bd3579fb7dcc284c5",
+    "url": "/react-display-name/static/js/2.3fe580a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-display-name/static/js/2.3fe580a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60839cc9f8b7b5d3e6da",
+    "url": "/react-display-name/static/js/main.2bb9e9b7.chunk.js"
+  },
+  {
+    "revision": "ab488b9fecb8999076f3",
+    "url": "/react-display-name/static/js/runtime-main.815c046d.js"
+  }
+]);
--- src/DisplayName/TrueAnonymousClazzComponent.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-
-// 🛑 display name: Component
-export default class extends Component {
-  render() {
-    return <div>AnonymousClazzComponent</div>;
-  }
-}
